1a9292bbf051893a3eb007600b7f433c1f3a97f6
Zakir-14 <71498578+Zakir-14@users.noreply.github.com>
2020-10-26 15:35:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground,groundIMG;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png");
+	packageIMG=loadImage("package.png");
+	groundIMG = loadImage("ground2.png");
+}	
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	
+	box1 = createSprite(330,655,150,20);
+	box1.shapeColor = "red";
+	box2 = createSprite(265,630,20,60);
+	box2.shapeColor = "red";
+	box3 = createSprite(395,630,20,60);
+	box3.shapeColor = "red";
+    
+
+	packageSprite=createSprite(0, 200, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+	packageSprite.visible = false;
+	//packageSprite.debug = true;
+	//packageSprite.velocityX = 4;
+
+	helicopterSprite=createSprite(0, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.scale=0.6;
+	helicopterSprite.velocityX = 4; 
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+	groundSprite.addImage(groundIMG);
+	
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	helicopterSprite = Bodies.rectangle(0,200,10,10,{isStatic:true});
+	World.add(world, helicopterSprite);
+	
+	packageBody = Bodies.rectangle(	335, 200 , 10,10 , {restitution:0.5, isStatic:true});
+	//Matter.Body.setVelocity(packageBody,{x:4,y:0})
+	World.add(world, packageBody);
+	
+	box1 = Bodies.rectangle(330,655,150,50,{isStatic:true})
+
+	World.add(world, box1);
+
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+
+if(keyDown(DOWN_ARROW)){
+	Matter.Body.setStatic(packageBody, false)
+	packageSprite.visible = true;
+}
+
+  packageSprite.x= packageBody.position.x;
+  packageSprite.y= packageBody.position.y; 
+  drawSprites();
+  textSize(20);
+ text("DROP THE PACKAGE IN THE BOX",300,100);
+ 
+}
+
+
+
+
